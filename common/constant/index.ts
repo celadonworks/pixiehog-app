672d2f4bdeabf33dd76eab0f43dfc5138cf9d625
Ahmed <contact.ahmedbm@gmail.com>
2024-10-19 20:11:55 +0100

    feat: add Constant file

---
+++ common/constant/index.ts
@@ -0,0 +1,5 @@
+export class Constant {
+  static readonly METAFIELD_NAMESPACE = "ph_analytics";
+  static readonly POSTHOG_METAFIELD_KEY = "apiKey";
+  static readonly WEBPIXEL_METAFIELD_KEY = "apiKey";
+}
