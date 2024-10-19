e4af44b972e6a5702e2994152dfdf3117c029bb5
Ahmed <contact.ahmedbm@gmail.com>
2024-10-19 20:11:30 +0100

    feat: add queryCurrentAppInstallation query

---
+++ app/common.server/queries/current-app-installation.ts
@@ -0,0 +1,50 @@
+import type { AdminApiContext } from "@shopify/shopify-app-remix/server";
+import type { ShopifyGraphQLResponse } from "../interfaces/shopify-graphql.interface";
+import { json } from "@remix-run/node";
+import type { Metafield } from "../interfaces/shopify-metafield-interface";
+
+
+export interface CurrentAppInstallationResponseDTO {
+  currentAppInstallation: CurrentAppInstallation
+}
+
+export interface CurrentAppInstallation {
+  id:  string
+  app: App
+  ph_key: Metafield
+  web_pixel_events: Metafield
+}
+
+export interface App {
+  id: string;
+}
+
+export const queryCurrentAppInstallation = async (admin:AdminApiContext) => {
+  const response = await admin.graphql(
+    `#graphql
+    query {
+      currentAppInstallation {
+        id
+        app {
+          id
+        }
+        ph_key: metafield(namespace: "ph_analytics", key: "apiKey") {
+          key
+          jsonValue
+          value
+          type
+        }
+        web_pixel_events: metafield(namespace: "ph_analytics", key: "webPixelEvents") {
+          key
+          jsonValue
+          value
+          type
+        }
+        
+      }
+    }
+    `
+  )
+  const responseJson = await response.json() as ShopifyGraphQLResponse<CurrentAppInstallationResponseDTO>
+  return json(responseJson.data.currentAppInstallation)
+}
