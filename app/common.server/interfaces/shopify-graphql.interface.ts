3e06a4c607d7d8b6b0d578fe0405b2553b03ff36
Ahmed <contact.ahmedbm@gmail.com>
2024-10-19 20:08:59 +0100

    feat(interfaces): add Shopify graphql response interface

---
+++ app/common.server/interfaces/shopify-graphql.interface.ts
@@ -0,0 +1,20 @@
+export interface ShopifyGraphQLResponse<T> {
+  data:       T;
+  extensions: Extensions;
+}
+
+export interface Extensions {
+  cost: Cost;
+}
+
+export interface Cost {
+  requestedQueryCost: number;
+  actualQueryCost: number;
+  throttleStatus: ThrottleStatus;
+}
+
+export interface ThrottleStatus {
+  maximumAvailable: number;
+  currentlyAvailable: number;
+  restoreRate: number;
+}
