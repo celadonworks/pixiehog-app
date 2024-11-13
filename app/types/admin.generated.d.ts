2d08d79cdc3ffcc21c570444019e4493d7a2dd13
Adam Boulila <adam.boulila@live.fr>
2024-11-13 11:04:37 +0000

    chore: update types for themes

--- app/types/admin.generated.d.ts
+++ app/types/admin.generated.d.ts
@@ -58,6 +58,21 @@ export type CurrentAppInstallationQuery = { currentAppInstallation: (
     & { app: Pick<AdminTypes.App, 'id'>, posthog_api_key?: AdminTypes.Maybe<Pick<AdminTypes.Metafield, 'key' | 'value' | 'type'>>, posthog_api_host?: AdminTypes.Maybe<Pick<AdminTypes.Metafield, 'key' | 'value' | 'type'>>, web_pixel_settings?: AdminTypes.Maybe<Pick<AdminTypes.Metafield, 'key' | 'jsonValue' | 'type'>>, web_pixel_feature_toggle?: AdminTypes.Maybe<Pick<AdminTypes.Metafield, 'key' | 'jsonValue' | 'value' | 'type'>>, js_web_posthog_config?: AdminTypes.Maybe<Pick<AdminTypes.Metafield, 'key' | 'jsonValue' | 'value' | 'type'>>, js_web_posthog_feature_toggle?: AdminTypes.Maybe<Pick<AdminTypes.Metafield, 'key' | 'jsonValue' | 'value' | 'type'>> }
   ) };
 
+export type ThemesQueryVariables = AdminTypes.Exact<{
+  first: AdminTypes.Scalars['Int']['input'];
+  files?: AdminTypes.InputMaybe<Array<AdminTypes.Scalars['String']['input']> | AdminTypes.Scalars['String']['input']>;
+  roles?: AdminTypes.InputMaybe<Array<AdminTypes.ThemeRole> | AdminTypes.ThemeRole>;
+}>;
+
+
+export type ThemesQuery = { themes?: AdminTypes.Maybe<{ nodes: Array<(
+      Pick<AdminTypes.OnlineStoreTheme, 'id' | 'name' | 'role'>
+      & { files?: AdminTypes.Maybe<{ nodes: Array<(
+          Pick<AdminTypes.OnlineStoreThemeFile, 'filename'>
+          & { body: Pick<AdminTypes.OnlineStoreThemeFileBodyText, 'content'> }
+        )> }> }
+    )> }> };
+
 export type WebPixelQueryVariables = AdminTypes.Exact<{ [key: string]: never; }>;
 
 
@@ -65,6 +80,7 @@ export type WebPixelQuery = { webPixel?: AdminTypes.Maybe<Pick<AdminTypes.WebPix
 
 interface GeneratedQueryTypes {
   "#graphql\n      query currentAppInstallation(\n        $namespace: String!\n        $posthogApiKeyKey: String!\n        $posthogApiHostKey: String!,\n        $webPixelEventsSettingsKey: String!\n        $webPixelFeatureToggle: String!\n        $jsWebPosthogConfig: String!\n        $jsWebPosthogFeatureToggle: String!\n      ) {\n        currentAppInstallation {\n          id\n          app {\n            id\n          }\n          posthog_api_key: metafield(namespace: $namespace, key: $posthogApiKeyKey) {\n            key\n            value\n            type\n          }\n          posthog_api_host: metafield(namespace: $namespace, key: $posthogApiHostKey) {\n            key\n            value\n            type\n          }\n          web_pixel_settings: metafield(namespace: $namespace, key: $webPixelEventsSettingsKey) {\n            key\n            jsonValue\n            type\n          }\n          web_pixel_feature_toggle: metafield(namespace: $namespace, key: $webPixelFeatureToggle) {\n            key\n            jsonValue\n            value\n            type\n          }\n          js_web_posthog_config: metafield(namespace: $namespace, key: $jsWebPosthogConfig) {\n            key\n            jsonValue\n            value\n            type\n          },\n          js_web_posthog_feature_toggle: metafield(namespace: $namespace, key: $jsWebPosthogFeatureToggle) {\n            key\n            jsonValue\n            value\n            type\n          },\n        }\n      }\n    ": {return: CurrentAppInstallationQuery, variables: CurrentAppInstallationQueryVariables},
+  "\n      #graphql\n      query Themes($first: Int!, $files: [String!], $roles: [ThemeRole!]) {\n        themes(first: $first, roles: $roles) {\n          nodes {\n            id\n            name\n            role\n            files(filenames: $files) {\n              nodes {\n                filename\n                body {\n                  ... on OnlineStoreThemeFileBodyText {\n                    content\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    ": {return: ThemesQuery, variables: ThemesQueryVariables},
   "#graphql\n        query webPixel {\n          webPixel {\n            id\n            settings\n          }\n        }\n      ": {return: WebPixelQuery, variables: WebPixelQueryVariables},
 }
 
