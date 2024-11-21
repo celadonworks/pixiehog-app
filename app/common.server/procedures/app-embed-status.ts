bfd10fec5509206323fe8332124be2ff75616387
Ahmed <contact.ahmedbm@gmail.com>
2024-11-21 15:01:51 +0000

    feat: iuse json5 to parse json string file with comments

--- app/common.server/procedures/app-embed-status.ts
+++ app/common.server/procedures/app-embed-status.ts
@@ -1,6 +1,7 @@
 import type { AdminGraphqlClient } from '@shopify/shopify-app-remix/server';
 import type { ThemeRole } from '../../types/admin.types';
 import { queryThemes } from '../queries/query-themes';
+import  JSON5 from "json5"
 
 export async function appEmbedStatus(graphq: AdminGraphqlClient, appEmbedUuid: string) {
   const themes = await queryThemes(graphq, {
@@ -27,8 +28,8 @@ export async function appEmbedStatus(graphq: AdminGraphqlClient, appEmbedUuid: s
   if (!settingsData) {
     return false;
   }
-
-  const parsed = JSON.parse(settingsData) as { current: {blocks: Record<string, { type: string; disabled?: boolean }>} };
+  
+  const parsed = JSON5.parse(settingsData) as { current: {blocks: Record<string, { type: string; disabled?: boolean }>} };
 
   return Object.values(parsed.current.blocks).some((payload) => {
     return payload.type.includes(appEmbedUuid) && !payload.disabled
