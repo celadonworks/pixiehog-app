692bcbc8b56e4a195014cb5ced14f6d721172bfb
Adam Boulila <adam.boulila@live.fr>
2024-12-01 16:47:56 +0000

    feat: add primitives to api host and api key

--- common/dto/posthog-api-host.dto.ts
+++ common/dto/posthog-api-host.dto.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
+export const posthogApiHostPrimitive = z.string().describe('PostHog API Host.').trim().url().default('')
+
 export const PosthogApiHostSchema = z.object({
-  posthog_api_host: z.string().describe('PostHog API Host.').trim().url().default('')
+  posthog_api_host: posthogApiHostPrimitive
 });
 
 export type PosthogApiHost = z.infer<typeof PosthogApiHostSchema>;
+export type PosthogApiHostPrimitive = z.infer<typeof posthogApiHostPrimitive>;
--- common/dto/posthog-api-key.dto.ts
+++ common/dto/posthog-api-key.dto.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
+export const posthogApiKeyPrimitive = z.string().trim().startsWith('phc_').or(z.literal(''))
+
 export const PosthogApiKeySchema = z.object({
-  posthog_api_key: z.string().trim().startsWith('phc_').or(z.literal('')),
+  posthog_api_key: posthogApiKeyPrimitive,
 });
 
 export type PosthogApiKey = z.infer<typeof PosthogApiKeySchema>;
+export type posthogApiKeyPrimitive = z.infer<typeof posthogApiKeyPrimitive>;
