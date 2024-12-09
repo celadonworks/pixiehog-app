b1cf180bd5f954efc72b1679adcb8286c4180296
Adam Boulila <adam.boulila@live.fr>
2024-12-09 05:04:46 +0000

    feat: setup posthog transition tracking

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,50 +1,12 @@
-import { RemixBrowser, useNavigation } from '@remix-run/react';
-import { useAppBridge } from '@shopify/app-bridge-react';
-import posthog from 'posthog-js';
-import { startTransition, StrictMode, useEffect } from 'react';
+import { RemixBrowser } from '@remix-run/react';
+import { startTransition, StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
-function PosthogInit() {
-  const shopify = useAppBridge();
-  const navigation = useNavigation();
-  useEffect(() => {
-    if (!window.ENV.POSTHOG_API_KEY) {
-      console.log('posthog disabled - no api key')
-      return;
-    }
-    posthog.init(window.ENV.POSTHOG_API_KEY, {
-      api_host: window.ENV.POSTOHG_API_HOST,
-      person_profiles: 'always',
-      capture_pageleave: false,
-      enable_recording_console_log: true,
-      persistence: 'localStorage',
-    });
-    posthog.identify(
-      posthog.get_distinct_id(),  // Replace 'distinct_id' with your user's unique identifier
-      { shop: shopify.config.shop, } // optional: set additional person properties
-    );
-  });
-
-  useEffect(() => {
-    if (navigation.state != 'idle') {
-      return
-    }
-    posthog.capture(
-      '$pageview',
-      {
-        '$current_url': window.location.href,
-      }
-    )
-  }, [navigation.state])
-
-  return null;
-}
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
       <RemixBrowser />
-      <PosthogInit />
     </StrictMode>
   );
 });
