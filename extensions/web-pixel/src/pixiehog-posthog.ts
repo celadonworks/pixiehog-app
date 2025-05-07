0e5e49dfcff4332058748aaa0337f3655b985079
Ahmed <contact.ahmedbm@gmail.com>
2025-05-07 19:29:22 +0200

    feat: version update

--- extensions/web-pixel/src/pixiehog-posthog.ts
+++ extensions/web-pixel/src/pixiehog-posthog.ts
@@ -1,12 +1,12 @@
-import { PostHog, } from 'posthog-node';
-import { version } from '../package.json';
-
+import { PostHog } from 'posthog-node';
+/* import { version } from '../package.json';
+ */
 export class PixieHogPostHog extends PostHog {
   getLibraryId(): string {
     return 'pixiehog';
   }
   getLibraryVersion(): string {
-    return version;
+    return '1.0.1';
   }
   getCustomUserAgent(): string {
     return `${super.getLibraryId()}/${super.getLibraryVersion()}`
